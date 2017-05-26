3e1dc64a6be2e40bc803e0fda1f607455b652584
ashishknl <ashish.knl.2000@gmail.com>
2017-05-27 00:13:01 +0530

    smapl file

---
+++ sample.php
@@ -0,0 +1,35 @@
+this is sample file 
+yes i have done it right
+Untitled-2.php
+
+exit
+
+
+ out
+exit>
+cd
+sdsd
+sd
+sd
+
+øøø
+
+ls
+ls -al
+:wq
+
+i
+:i
+:wq
+q
+
+man less
+h
+man
+man xyz
+cat > exit
+:exit
+q!
+:
+reset followed by ctrl-j
+
